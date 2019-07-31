b582645903b91b3df11fac95c64cab19afcb34bc
George Spyrou <gspyrou@umich.edu>
2019-07-31 21:22:42 +0100

    Update Unique_Morse_Code_Words.py

--- Easy/Unique_Morse_Code_Words.py
+++ Easy/Unique_Morse_Code_Words.py
@@ -1,3 +1,14 @@
+'''
+Now, given a list of words, each word can be written as a concatenation of the Morse code of each letter. For example, "cba" can be written as "-.-..--...", (which is the concatenation "-.-." + "-..." + ".-"). We'll call such a concatenation, the transformation of a word.
+
+Return the number of different transformations among all words we have.
+
+Example:
+Input: words = ["gin", "zen", "gig", "msg"]
+Output: 2
+
+'''
+
 class Solution(object):
     def uniqueMorseRepresentations(self, words):
         """
