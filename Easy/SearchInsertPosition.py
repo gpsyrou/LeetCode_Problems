3d4de4f431d3b965ea097d6ee2a54f7c1f1952b2
George Spyrou <gspyrou@umich.edu>
2020-05-19 21:34:14 +0100

    Update SearchInsertPosition.py

--- Easy/SearchInsertPosition.py
+++ Easy/SearchInsertPosition.py
@@ -7,11 +7,15 @@ You may assume no duplicates in the array.
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
         for index, number in enumerate(nums):
+            # Check if is should go in the first position (0)
+            if number == 0:
+                return 0
             # Check if the number matches the target - if yes return the index
             if number == target:
                 return index
             else:
                 # Check if the next number is higher
-                if nums[index+1] > target:
+                if nums[index + 1] > target:
                     return index
-                elif 
+                elif index == (len(nums) - 1):
+                    return index + 1
