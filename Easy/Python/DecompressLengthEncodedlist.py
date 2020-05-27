063d1a9d88b1d32e70d7e227b111ba6abae762c6
George Spyrou <gspyrou@umich.edu>
2020-05-27 19:57:32 +0100

    add problem 1313 in python

---
+++ Easy/Python/DecompressLengthEncodedlist.py
@@ -0,0 +1,15 @@
+''' 1313. Decompress Run-Length Encoded List
+
+We are given a list nums of integers representing a list compressed with run-length encoding.
+
+Consider each adjacent pair of elements [freq, val] = [nums[2*i], nums[2*i+1]] (with i >= 0).  For each such pair, there are freq elements with value val concatenated in a sublist. Concatenate all the sublists from left to right to generate the decompressed list.
+'''
+class Solution:
+    def decompressRLElist(self, nums: List[int]) -> List[int]:
+        decompressed_ls = []
+        
+        for i in range(0, len(nums), 2):
+            decompressed_ls += [nums[i+1] for x in range(nums[i])]
+        return decompressed_ls
+
+
