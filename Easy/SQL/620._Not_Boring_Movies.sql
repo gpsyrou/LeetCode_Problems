113fd4f08e03f95f111e20714758c42a62dfcca6
George Spyrou <gspyrou@umich.edu>
2020-09-05 21:38:14 +0100

    Create 620._Not_Boring_Movies.sql

---
+++ Easy/SQL/620._Not_Boring_Movies.sql
@@ -0,0 +1,9 @@
+/* 620. Not Boring Movies
+
+X city opened a new cinema, many people would like to go to this cinema. The cinema also gives out a poster indicating the movies’ ratings and descriptions.
+Please write a SQL query to output movies with an odd numbered ID and a description that is not 'boring'. Order the result by rating.
+*/
+SELECT *
+FROM cinema
+WHERE [ID]%2 != 0 AND [Description] != 'boring'
+ORDER BY [Rating] DESC
